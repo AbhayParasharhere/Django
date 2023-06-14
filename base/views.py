64b260f50519053631ee4f85e2fd2c51f81bcaf9
Abhay <abhayparasharr@gmail.com>
2023-06-14 08:19:58 -0700

    Create a template and include navbar content in home page

--- base/views.py
+++ base/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def home(Request):
-    return HttpResponse('Home Page')
+def home(request):
+    return render(request, 'home.html')
 
 
-def room(Request):
-    return HttpResponse('Room')
+def room(request):
+    return render(request, 'room.html')
